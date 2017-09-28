481b5ea1da49cd817c685834d1cba14afc976720
Dmitriy Krivoruchko <dkrivoruchko@gmail.com>
2017-09-28 11:21:32 +0300

    Update FakeSocket: Add statuses & async storing

--- data/src/main/java/info/dvkr/primenumbers/data/primenumber/repository/datasource/FakeSocket.java
+++ data/src/main/java/info/dvkr/primenumbers/data/primenumber/repository/datasource/FakeSocket.java
@@ -5,29 +5,69 @@ import android.support.annotation.NonNull;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.SocketException;
 import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import info.dvkr.primenumbers.domain.model.PrimeNumber;
 
 public class FakeSocket {
+    volatile private boolean isConnected;
+    volatile private boolean isClosed;
+
+    // Do not inject via Dagger because it's a demo fake implementation
+    private ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
     private Random r = new Random();
     private FileOutputStream outputStream;
 
+    // Using Future<PrimeNumber> to implement socket read/write delay without blocking thread
     public FakeSocket(final Context context,
                       @NonNull String filename) throws IOException {
         outputStream = context.openFileOutput(filename, Context.MODE_APPEND);
     }
 
-    public void write(String string) throws IOException {
-        try {
-            Thread.sleep(100); // Writing slowly
-        } catch (InterruptedException ignore) {
-        }
-
-        // Make random network error
-        if (r.nextInt(10) < 1) throw new IOException("This is demo network error.");
-        outputStream.write(string.getBytes());
+    public void connect() throws IOException {
+        if (isConnected) throw new SocketException("Socket is connected");
+        if (isClosed) throw new SocketException("Socket is closed");
+        isConnected = true;
     }
 
-    public void close() throws IOException {
+    public Future<Object> write(final PrimeNumber primeNumber) throws RejectedExecutionException {
+        return scheduler.schedule(new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+                if (!isConnected) throw new SocketException("Socket is not connected");
+                if (isClosed) throw new SocketException("Socket is closed");
+
+                if (r.nextInt(20) < 1) { // Make random network error
+                    close(); // Closing socket
+                    throw new IOException("This is demo network error.");
+                }
+                outputStream.write((primeNumber.toString() + "\n").getBytes());
+                return null;
+            }
+        }, r.nextInt(100), TimeUnit.MILLISECONDS); // Random delay - simulating slow connection
+    }
+
+    public synchronized void close() throws IOException {
+        if (!isConnected) throw new SocketException("Socket is not connected");
+        if (isClosed) throw new SocketException("Socket is closed");
+
         outputStream.close();
+        scheduler.shutdownNow();
+        isClosed = true;
+    }
+
+    public boolean isConnected() {
+        return isConnected;
+    }
+
+    public boolean isClosed() {
+        return isClosed;
     }
 }
--- data/src/main/java/info/dvkr/primenumbers/data/primenumber/repository/datasource/NetworkDataStore.java
+++ data/src/main/java/info/dvkr/primenumbers/data/primenumber/repository/datasource/NetworkDataStore.java
@@ -6,11 +6,15 @@ import android.support.annotation.Nullable;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -20,7 +24,7 @@ public class NetworkDataStore {
 
     // Prime number queue max length.
     // In real application can be adjusted base on memory requirements
-    private final int MAX_PRIME_NUMBER_QUEUE_LENGTHS = 100;
+    private final int MAX_PRIME_NUMBER_QUEUE_LENGTHS = 500;
 
     private final BlockingQueue<PrimeNumber> primeNumberQueue =
             new LinkedBlockingQueue<>(MAX_PRIME_NUMBER_QUEUE_LENGTHS);
@@ -28,80 +32,128 @@ public class NetworkDataStore {
     // Use this for saving numbers
     private final ConcurrentLinkedQueue<PrimeNumber> primeNumbers = new ConcurrentLinkedQueue<>();
 
+
     @Nullable
     private DataStoreCallback callback;
 
+    private static class PrimeNumberFuture {
+        private final PrimeNumber primeNumber;
+        private final Future<Object> primeNumberFuture;
+        private final FakeSocket fakeSocket;
+
+        public PrimeNumberFuture(final PrimeNumber primeNumber, final Future<Object> primeNumberFuture, final FakeSocket fakeSocket) {
+            this.primeNumber = primeNumber;
+            this.primeNumberFuture = primeNumberFuture;
+            this.fakeSocket = fakeSocket;
+        }
+
+
+        public PrimeNumber getPrimeNumber() {
+            return primeNumber;
+        }
+
+        public Future<Object> getPrimeNumberFuture() {
+            return primeNumberFuture;
+        }
+
+        public FakeSocket getFakeSocket() {
+            return fakeSocket;
+        }
+    }
+
+    private final List<PrimeNumberFuture> primeNumbersFutures = new LinkedList<>();
 
     public NetworkDataStore(final Context context, // For saving file,
                             @NonNull final ExecutorService executor) {
-
         executor.execute(new Runnable() {
             @Override
             public void run() {
                 Thread.currentThread().setName("NetworkDataStore");
 
+                PrimeNumber primeNumberToSave;
                 FakeSocket fakeSocket = null;
-                boolean isWriteSuccess;
-                int retryCount;
+
+                int retryCount = 0;
                 while (!Thread.currentThread().isInterrupted()) {
-                    try {
-                        final PrimeNumber primeNumber = primeNumberQueue.poll(10, TimeUnit.MILLISECONDS);
-                        if (primeNumber == null) continue;
-
-                        isWriteSuccess = false;
-                        retryCount = 0;
-                        while (true) {
-                            try {
-                                if (fakeSocket == null)
-                                    fakeSocket = new FakeSocket(context, "PrimeNumbers.txt");
-
-                                fakeSocket.write(primeNumber.toString() + "\n");
-
-                                primeNumbers.add(primeNumber);
-                                if (callback != null) callback.onDataStoreUpdate();
-                                isWriteSuccess = true;
-
-                            } catch (IOException e) { // We have error
-                                if (callback != null)
-                                    callback.onNetworkError(new IOException("NetError on saving: " + primeNumber + " Retry: " + retryCount, e));
-
-                                // Closing current connection
-                                if (fakeSocket != null) {
-                                    try {
-                                        fakeSocket.close();
-                                    } catch (IOException ex) {
-                                        if (callback != null) callback.onNetworkError(ex);
-                                    }
-                                    fakeSocket = null;
-                                }
-                                retryCount++;
-                            }
-
-                            if (isWriteSuccess) break;
-                            if (retryCount >= 3) {
-                                if (callback != null)
-                                    callback.onNetworkError(new IOException("Too many network errors"));
-                                if (fakeSocket != null) {
-                                    try {
-                                        fakeSocket.close();
-                                    } catch (IOException ex) {
-                                        if (callback != null) callback.onNetworkError(ex);
-                                    }
-                                }
-                                return;
-                            }
-                        }
+                    primeNumberToSave = null;
+                    try { // getting net number to save
+                        primeNumberToSave = primeNumberQueue.poll(10, TimeUnit.MILLISECONDS);
                     } catch (InterruptedException ignore) {
                     }
-                }
 
-                if (fakeSocket != null) {
-                    try {
-                        fakeSocket.close();
-                    } catch (IOException ex) {
-                        if (callback != null) callback.onNetworkError(ex);
+                    // Sending number to socket
+                    if (primeNumberToSave != null) {
+                        try {
+                            if (fakeSocket == null) {
+                                fakeSocket = new FakeSocket(context, "PrimeNumbers.txt");
+                                fakeSocket.connect();
+                            }
+                            final Future<Object> primeNumberFuture = fakeSocket.write(primeNumberToSave);
+                            primeNumbersFutures.add(new PrimeNumberFuture(primeNumberToSave, primeNumberFuture, fakeSocket));
+                        } catch (IOException | RejectedExecutionException ex) {
+                            retryCount++;
+
+                            if (fakeSocket != null && fakeSocket.isConnected() && !fakeSocket.isClosed())
+                                try {
+                                    fakeSocket.close();
+                                } catch (IOException e) {
+                                    if (callback != null) callback.onNetworkError(e);
+                                }
+                            fakeSocket = null;
+
+                            addPrimeNumber(primeNumberToSave); // Queering to store again
+                            if (callback != null)
+                                callback.onNetworkError(new IOException("NetError on saving: " + primeNumberToSave));
+                        }
+                    }
+
+                    // Checking current futures results
+                    PrimeNumber primeNumberAfterSave;
+                    for (int i = 0; i < primeNumbersFutures.size(); i++) {
+                        if (primeNumbersFutures.get(i).getPrimeNumberFuture().isDone()) { // Task is completed of failed
+                            final PrimeNumberFuture primeNumberFuture = primeNumbersFutures.remove(i);
+                            primeNumberAfterSave = null;
+                            try {
+                                primeNumberAfterSave = primeNumberFuture.getPrimeNumber();
+                                primeNumberFuture.getPrimeNumberFuture().get();
+                                // No exception, successful store
+                                primeNumbers.add(primeNumberAfterSave); // Adding to local cache
+                                if (callback != null) callback.onDataStoreUpdate();
+                            } catch (InterruptedException | ExecutionException e) {
+                                // Store error
+                                retryCount++;
+                                addPrimeNumber(primeNumberAfterSave); // Queering to store again
+                                if (callback != null)
+                                    callback.onNetworkError(new IOException("NetError on saving: " + primeNumberAfterSave));
+                                fakeSocket = null;
+                            }
+                        } else if (primeNumbersFutures.get(i).getFakeSocket() != fakeSocket) {
+                            // Socket failed for this task
+                            final PrimeNumberFuture primeNumberFuture = primeNumbersFutures.remove(i);
+                            addPrimeNumber(primeNumberFuture.getPrimeNumber()); // Queering to store again
+                        }
+                    }
+
+                    if (retryCount >= 20) {
+                        if (callback != null)
+                            callback.onNetworkError(new IOException("Too many network errors"));
+                        if (fakeSocket != null && fakeSocket.isConnected() && !fakeSocket.isClosed()) {
+                            try {
+                                fakeSocket.close();
+                            } catch (IOException ex) {
+                                if (callback != null) callback.onNetworkError(ex);
+                            }
+                        }
+                        return;
                     }
                 }
+
+                if (fakeSocket != null && fakeSocket.isConnected() && !fakeSocket.isClosed())
+                    try {
+                        fakeSocket.close();
+                    } catch (IOException e) {
+                        if (callback != null) callback.onNetworkError(e);
+                    }
             }
         });
     }
